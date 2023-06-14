7ef2714a1f4b54c176a049f8dce62941d8bd20d1
荒木英雄 <arakihideo@myStardustMachine.local>
2023-06-14 11:23:15 +0900

    add login conponent and sass

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,28 @@ import React from "react";
 import "./App.scss";
 import Sidebar from "./components/sidebar/Sidebar";
 import Chat from "./components/chat/Chat";
+import { useSelector } from "react-redux";
+import Login from "./components/Login/Login";
 
 function App() {
+  // const user = useSelector((state) => state.user.user);
+  const user = null; // TODO
+
   return (
     <div className="App">
-      {/* sidebar */}
-      <Sidebar />
+      {user ? (
+        <>
+          {/* sidebar */}
+          <Sidebar />
 
-      {/* char */}
-      <Chat />
+          {/* char */}
+          <Chat />
+        </>
+      ) : (
+        <>
+          <Login />
+        </>
+      )}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
