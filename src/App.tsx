4d84c332924265e9c5656d4d1c04ff64822131b3
荒木英雄 <arakihideo@myStardustMachine.local>
2023-06-14 11:38:22 +0900

    add useAppDispatch and useAppSelector

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Chat from "./components/chat/Chat";
 import { useSelector } from "react-redux";
 import Login from "./components/Login/Login";
+import { useAppSelector } from "./app/hooks";
 
 function App() {
-  // const user = useSelector((state) => state.user.user);
-  const user = null; // TODO
+  const user = useAppSelector((state) => state.user);
+  // const user = null; // TODO
 
   return (
     <div className="App">
